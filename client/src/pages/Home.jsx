bc1a4ac1e1745a9320eb2a36fa5dc662eb702bb5
Afacehacker <afaceabolade@gmail.com>
2026-03-11 00:11:55 +0100

    fix(client): ReferenceError Link is not defined in Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import API from '../services/api';
 import ProductCard from '../components/ProductCard';
 import { AuthContext } from '../context/AuthContext';
