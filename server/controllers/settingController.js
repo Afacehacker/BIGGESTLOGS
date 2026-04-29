2d43869c9aa1d49ba784ff8917db8556ab9bdb46
Afacehacker <afaceabolade@gmail.com>
2026-04-29 01:10:27 +0100

    fix: remove express-async-handler dependency from settingsController

--- server/controllers/settingController.js
+++ server/controllers/settingController.js
@@ -1,35 +1,42 @@
-const asyncHandler = require('express-async-handler');
 const Setting = require('../models/Setting');
 
 // @desc    Get settings
 // @route   GET /api/settings
 // @access  Public
-const getSettings = asyncHandler(async (req, res) => {
-    let settings = await Setting.findOne();
-    if (!settings) {
-        settings = await Setting.create({});
+const getSettings = async (req, res, next) => {
+    try {
+        let settings = await Setting.findOne();
+        if (!settings) {
+            settings = await Setting.create({});
+        }
+        res.json(settings);
+    } catch (error) {
+        next(error);
     }
-    res.json(settings);
-});
+};
 
 // @desc    Update settings
 // @route   PUT /api/settings
 // @access  Private/Admin
-const updateSettings = asyncHandler(async (req, res) => {
-    let settings = await Setting.findOne();
-    
-    if (!settings) {
-        settings = new Setting();
+const updateSettings = async (req, res, next) => {
+    try {
+        let settings = await Setting.findOne();
+        
+        if (!settings) {
+            settings = new Setting();
+        }
+
+        settings.bankName = req.body.bankName || settings.bankName;
+        settings.accountName = req.body.accountName || settings.accountName;
+        settings.accountNumber = req.body.accountNumber || settings.accountNumber;
+        settings.telegramLink = req.body.telegramLink || settings.telegramLink;
+
+        const updatedSettings = await settings.save();
+        res.json(updatedSettings);
+    } catch (error) {
+        next(error);
     }
-
-    settings.bankName = req.body.bankName || settings.bankName;
-    settings.accountName = req.body.accountName || settings.accountName;
-    settings.accountNumber = req.body.accountNumber || settings.accountNumber;
-    settings.telegramLink = req.body.telegramLink || settings.telegramLink;
-
-    const updatedSettings = await settings.save();
-    res.json(updatedSettings);
-});
+};
 
 module.exports = {
     getSettings,
